b47e9d910672658c36fc0e79f8bea94ce79d3caf
Lucas Mace <lucasmace4130@gmail.com>
2021-05-27 00:14:09 -0500

    Nav turns on in mobile

--- public/index.js
+++ public/index.js
@@ -1,15 +1,18 @@
 console.log("Thanks for looking!");
 
-let mode = 0;
 const button = document.getElementById("mode-toggle");
 const body = document.body;
 const h1 = document.getElementsByTagName("h1")[0];
 const h2 = document.getElementsByTagName("h2")[0];
 const p = document.getElementsByTagName("p");
 const link = document.getElementsByClassName("white-link")[0];
+const nav = document.getElementsByTagName("nav")[0];
+const hamburger = document.getElementById("hamburger");
 
-const toggle = () => {
-    if (mode === 0) {
+const mediaQuery = window.matchMedia('(min-width: 850px)');
+
+const styleToggle = () => {
+    if (button.innerHTML === "Color Mode") {
         toggleColor();
     } else {
         toggleDark();
@@ -23,7 +26,14 @@ const toggleColor = () => {
     h1.style.color = "yellow";
     h2.style.color = "yellow";
     link.style.color = "black";
+    hamburger.style.backgroundColor = "gold";
     
+    if (nav.style.display != "flex") {
+        nav.style.backgroundColor = "pink";
+    } else {
+        nav.style.backgroundColor = "gold";
+    }
+
     for (let i = 0; i < p.length; i++) {
         p[i].style.color = "black";
     }
@@ -36,8 +46,29 @@ const toggleDark = () => {
     h1.style.color = "white";
     h2.style.color = "white";
     link.style.color = "white";
-    
+    hamburger.style.backgroundColor = "slategray";
+    nav.style.backgroundColor = "darkslategray";
+
     for (let i = 0; i < p.length; i++) {
         p[i].style.color = "white";
     }
 }
+
+let mobileMenu = 0;
+
+const menuToggle = () => {
+    if (nav.style.display != "flex") {
+        nav.style.display = "flex";
+    } else {
+        nav.style.display = "none";
+    }
+}
+
+// const windowSize = (e) => {
+//   if (e.matches) {
+//     nav.style.display = "flex";
+//   }
+// }
+
+// mediaQuery.addListener(windowSize);
+// windowSize(mediaQuery);
