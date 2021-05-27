1aa9eff0714c6162a3387c1b44be5ba5de9d3b70
Lucas Mace <lucasmace4130@gmail.com>
2021-05-26 23:12:28 -0500

    Dark / Color Mode Toggle Button

--- public/index.js
+++ public/index.js
@@ -1,1 +1,43 @@
 console.log("Thanks for looking!");
+
+let mode = 0;
+const button = document.getElementById("mode-toggle");
+const body = document.body;
+const h1 = document.getElementsByTagName("h1")[0];
+const h2 = document.getElementsByTagName("h2")[0];
+const p = document.getElementsByTagName("p");
+const link = document.getElementsByClassName("white-link")[0];
+
+const toggle = () => {
+    if (mode === 0) {
+        toggleColor();
+    } else {
+        toggleDark();
+    }
+}
+
+const toggleColor = () => {
+    mode = 1;
+    button.innerHTML = "Dark Mode";
+    body.style.backgroundColor = "pink";
+    h1.style.color = "yellow";
+    h2.style.color = "yellow";
+    link.style.color = "black";
+    
+    for (let i = 0; i < p.length; i++) {
+        p[i].style.color = "black";
+    }
+}
+
+const toggleDark = () => {
+    mode = 0;
+    button.innerHTML = "Color Mode";
+    body.style.backgroundColor = "darkslategray";
+    h1.style.color = "white";
+    h2.style.color = "white";
+    link.style.color = "white";
+    
+    for (let i = 0; i < p.length; i++) {
+        p[i].style.color = "white";
+    }
+}
